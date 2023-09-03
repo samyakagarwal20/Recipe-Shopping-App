caf5163c6874aea1c48256ed2c6f31a8eb2dad3e
samyak <samyakagarwal20@gmail.com>
2023-09-03 13:41:26 +0530

    enhanced the recipe-list component

--- recipe-shopping-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-shopping-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+
+  recipes: Recipe[] = [
+    new Recipe(
+      'Crispy Corn',
+      'A blend of spices with crispy corn',
+      'https://static.toiimg.com/thumb/93714345.cms?imgsize=72836&width=800&height=800'
+    ),
+    new Recipe(
+      'Momos',
+      'Steamed, filled dumplings with sauce',
+      'https://images.travelandleisureasia.com/wp-content/uploads/sites/2/2022/06/08172820/Untitled-design-2-1.jpg?tr=w-1200,h-900'
+    )
+  ];
 
 }
